54a3e8309d0db358627b11d43e081477784b5a8f
Joseph Renor Obrino <obrino.josephrenor@nwssu.edu.ph>
2026-06-20 22:56:50 +0800

    Fix production admin API

--- src/pages/AdminDashboardPage.jsx
+++ src/pages/AdminDashboardPage.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import axios from "axios";
+import api from "../services/api";
 
 import {
   getAdminToken,
@@ -20,8 +20,8 @@ export default function AdminDashboardPage() {
   const loadDashboard = async () => {
     try {
       const response =
-        await axios.get(
-          "http://localhost:5000/api/admin/dashboard",
+       await api.get(
+        "/admin/dashboard",
           {
             headers: {
               Authorization: `Bearer ${getAdminToken()}`,
